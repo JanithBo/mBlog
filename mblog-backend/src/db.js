c5dd8ebf3c1c4c63d1c330922ed88227b6d9ccbf
Janith Bojitha <72045158+JanithBo@users.noreply.github.com>
2024-07-12 21:21:59 +0530

    project completed

---
+++ mblog-backend/src/db.js
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb';
+
+let db;
+
+async function connectToDb(cb) {
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.PASSWORD}@cluster0.sjecldz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    await client.connect();
+    db = client.db('react-blog-db');
+    cb();
+}
+
+export {
+    db,
+    connectToDb,
+};
